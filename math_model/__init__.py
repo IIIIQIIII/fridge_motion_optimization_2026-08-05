b87fd4299d1612b346f24490c4a5bf0bf79563aa
MASHIJIAN <73766507+IIIIQIIII@users.noreply.github.com>
2026-08-05 08:26:03 +0800

    Export centroidal stepping APIs

--- math_model/__init__.py
+++ math_model/__init__.py
@@ -1,11 +1,55 @@
 """Mathematical models for G1 refrigerator-opening motion optimization."""
 
-from .horizon import difference_residuals, joint_limit_barrier, joint_limit_margin, manipulability_metrics, minimum_jerk
-from .quasistatic import ContactEquilibriumResult, DoorResistanceModel, FootPatch, fit_hinge_circle, required_tangential_hand_force, solve_contact_equilibrium
+from .centroidal import solve_centroidal_equilibrium
+from .horizon import (
+    difference_residuals,
+    joint_limit_barrier,
+    joint_limit_margin,
+    manipulability_metrics,
+    minimum_jerk,
+)
+from .quasistatic import (
+    ContactEquilibriumResult,
+    DoorResistanceModel,
+    FootPatch,
+    fit_hinge_circle,
+    required_tangential_hand_force,
+    solve_contact_equilibrium,
+)
+from .stepping import (
+    FootPose,
+    StepCandidate,
+    StepPlannerConfig,
+    StepPlanningProblem,
+    StepReference,
+    SupportMode,
+    candidate_set,
+    detect_step_trigger,
+    generate_step_reference,
+    plan_best_single_step,
+)
 
 __all__ = [
-    "ContactEquilibriumResult", "DoorResistanceModel", "FootPatch",
-    "difference_residuals", "fit_hinge_circle", "joint_limit_barrier",
-    "joint_limit_margin", "manipulability_metrics", "minimum_jerk",
-    "required_tangential_hand_force", "solve_contact_equilibrium",
+    "ContactEquilibriumResult",
+    "DoorResistanceModel",
+    "FootPatch",
+    "FootPose",
+    "StepCandidate",
+    "StepPlannerConfig",
+    "StepPlanningProblem",
+    "StepReference",
+    "SupportMode",
+    "candidate_set",
+    "detect_step_trigger",
+    "difference_residuals",
+    "fit_hinge_circle",
+    "generate_step_reference",
+    "joint_limit_barrier",
+    "joint_limit_margin",
+    "manipulability_metrics",
+    "minimum_jerk",
+    "plan_best_single_step",
+    "required_tangential_hand_force",
+    "solve_centroidal_equilibrium",
+    "solve_contact_equilibrium",
 ]
